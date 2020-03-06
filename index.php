93bca92d7d1eac2c3f7c6452429f0a561f51150f
Joel Heinis <joel.heinis@uha.fr>
2020-03-06 16:50:39 +0100

    Class Router + Corrections redirection

--- index.php
+++ index.php
@@ -16,63 +16,8 @@ define('CLASSES', ROOT.DS.'classes');
 
 
 // =====================  Détermination du controleur à utiliser: Est-ce que j'ai un paramètre 'url' dans mon URL?
-if (isset($_GET['url'])){
-	//Il y a un paramètre de précisé.
-  $initialurl=isset($_GET['url'])?trim($_GET['url']):'';
-  //Nettoyage de l'url
-  if (substr($initialurl,-1) == '/')  $initialurl=substr($initialurl,0,-1);
-  if (substr($initialurl, 0,1) == '/')  $initialurl=substr($initialurl,1);
-}else{
-	//Pas de paramètre
-	$initialurl='';
-}
-
-$url = (!empty($initialurl)) ? explode('/', $initialurl) : array();
-//print_r($url); 0 controller, 1 method, other :parameters
-switch (count($url)) {
-  case 0:
-    $method='index';
-    $controller='home';
-    $id=null;
-    break;
-  case 1:
-    // Seul le Controller est fourni
-    $method='index';
-    $controller=ucfirst(strtolower($url[0]));
-    $id=null;
-    break;
-
-  case 2:
-    // Controller + Method fournis
-    $controller=ucfirst(strtolower($url[0]));
-    $method=$url[1];
-    $id=null;
-    break;
-  case 3:
-      // Controller + Method + id fournis
-      $controller=ucfirst(strtolower($url[0]));
-      $method=$url[1];
-      $id=$url[2];
-      break;
-  default:
-    //plus de paramètres ... il faut décider quoi faire
-    break;
-}
-
-// =====================  Appel
-//On construit le nom du fichier qui contient le contrôleur appelé (ou le contrôleur par défaut)
-$controllerfilename=$controller.'C.php';
-//On inclut les fichiers nécessaires
-include CONTROLLERS.DS.$controllerfilename;
-//On construit le nom de la classe que l'on va instancier
-$controllerclassname=ucfirst($controller).'Controller';
-//On instancie cette classe
-$c=new $controllerclassname();
-//On appelle la méthode demandée (ou la méthode par défaut)
-if (!is_null($id))
-  $c->$method($id);
-else
-  $c->$method();
-
-
+require_once CLASSES.DS.'router.php';
+$r = new Router();
+$r->url();
+$r->exec();
 ?>
