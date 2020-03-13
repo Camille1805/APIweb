93ec7bfb61fc0a6e6945f1ac9b24f83f3dad0636
Joel Heinis <joel.heinis@laposte.net>
2020-03-13 05:11:29 +0100

    Réorganisation du repo

---
+++ app/index.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(E_STRICT | E_ALL);
+date_default_timezone_set('Europe/Paris');
+setlocale (LC_TIME, 'fr_FR.utf8','fra');
+
+
+// =====================  Initialisation
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__));
+define('URL_BASE', 'http://'.$_SERVER['SERVER_NAME'].(($_SERVER['SERVER_PORT'] == '80')?'':':'.$_SERVER['SERVER_PORT']).((dirname($_SERVER['SCRIPT_NAME']) == DS)?'':dirname($_SERVER['SCRIPT_NAME'])) );
+define('CONTROLLERS', ROOT.DS.'controllers');
+define('VIEWS', ROOT.DS.'views');
+define('MODELS', ROOT.DS.'models');
+define('VENDORS', ROOT.DS.'vendors');
+define('CLASSES', ROOT.DS.'classes');
+
+
+// =====================  Détermination du controleur à utiliser: Est-ce que j'ai un paramètre 'url' dans mon URL?
+require_once CLASSES.DS.'router.php';
+$r = new Router();
+$r->url();
+$r->exec();
+?>
---
+++ app/models/employeeM.php
@@ -0,0 +1,111 @@
+<?php
+class EmployeeModel {
+    public function construct(){}
+    public function listAll(){
+      $sql='select E.EmployeeID, C.ContactID, E.NationalIDNumber,E.Title as ETitle, C.Title as CTitle, C.FirstName, C.MiddleName, C.LastName, C.EmailAddress, E.HireDate
+      from employee as E
+      inner join contact as C on E.ContactID=C.ContactID
+      where E.CurrentFlag<>0';
+      try {
+        $dbh = new PDO('mysql:host=localhost;dbname=adw;charset=utf8', 'root', '');
+        $stmt=$dbh->prepare($sql);
+        //$stmt->bindParam(":var",$var);
+        $res=($stmt->execute())?$stmt->fetchAll(PDO::FETCH_OBJ): null;
+        $dbh = null;
+        return $res;
+      } catch (PDOException $e) {
+          print "Erreur !: " . $e->getMessage() . "<br/>";
+          die();
+      }
+    }
+    public function listOne($id){
+      $sql='select E.*, C.*,E.Title as ETitle, C.Title as CTitle, EM.Title as EMTitle, CM.Title as CMTitle, CM.FirstName as CMFirstName, CM.MiddleName as CMMiddleName, CM.LastName as CMLastName
+      from employee as E
+      inner join contact as C on E.ContactID=C.ContactID
+      left join employee as EM on E.ManagerID=EM.EmployeeID
+      left join contact as CM on EM.ContactID=CM.ContactID
+      where E.CurrentFlag<>0
+      and E.EmployeeID=:id';
+      try {
+        $dbh = new PDO('mysql:host=localhost;dbname=adw;charset=utf8', 'root', '');
+        $stmt=$dbh->prepare($sql);
+        $stmt->bindParam(":id",$id);
+        $res=($stmt->execute())?$stmt->fetchAll(PDO::FETCH_OBJ): null;
+        $dbh = null;
+        return current($res);
+      } catch (PDOException $e) {
+          print "Erreur !: " . $e->getMessage() . "<br/>";
+          die();
+      }
+    }
+    public function listAllFromDepartment($id){
+      $sql='select E.EmployeeID, C.ContactID, E.NationalIDNumber,E.Title as ETitle, C.Title as CTitle, C.FirstName, C.MiddleName, C.LastName, C.EmailAddress, E.HireDate
+      from employee as E
+      inner join contact as C on E.ContactID=C.ContactID
+      inner join employeedepartmenthistory as EDH on E.EmployeeID=EDH.EmployeeID
+        and EDH.DepartmentID=:id
+      where E.CurrentFlag<>0';
+      try {
+        $dbh = new PDO('mysql:host=localhost;dbname=adw;charset=utf8', 'root', '');
+        $stmt=$dbh->prepare($sql);
+        $stmt->bindParam(":id",$id);
+        $res=($stmt->execute())?$stmt->fetchAll(PDO::FETCH_OBJ): null;
+        $dbh = null;
+        return $res;
+      } catch (PDOException $e) {
+          print "Erreur !: " . $e->getMessage() . "<br/>";
+          die();
+      }
+    }
+    public function listManagers(){
+      $sql='select E.EmployeeID, C.ContactID, E.NationalIDNumber,E.Title as ETitle, C.Title as CTitle, C.FirstName, C.MiddleName, C.LastName, C.EmailAddress, E.HireDate
+      from employee as E
+      inner join contact as C on E.ContactID=C.ContactID
+      where E.CurrentFlag<>0
+      and E.EmployeeID in (select distinct M.ManagerID from employee as M where not isnull(M.ManagerID))';
+      try {
+        $dbh = new PDO('mysql:host=localhost;dbname=adw;charset=utf8', 'root', '');
+        $stmt=$dbh->prepare($sql);
+        //$stmt->bindParam(":var",$var);
+        $res=($stmt->execute())?$stmt->fetchAll(PDO::FETCH_OBJ): null;
+        $dbh = null;
+        return $res;
+      } catch (PDOException $e) {
+          print "Erreur !: " . $e->getMessage() . "<br/>";
+          die();
+      }
+    }
+    public function listWithAManager($id){
+      $sql='select E.EmployeeID, C.ContactID, E.NationalIDNumber,E.Title as ETitle, C.Title as CTitle, C.FirstName, C.MiddleName, C.LastName, C.EmailAddress, E.HireDate
+      from employee as E
+      inner join contact as C on E.ContactID=C.ContactID
+      where E.CurrentFlag<>0
+      and E.ManagerId=:id';
+      try {
+        $dbh = new PDO('mysql:host=localhost;dbname=adw;charset=utf8', 'root', '');
+        $stmt=$dbh->prepare($sql);
+        $stmt->bindParam(":id",$id);
+        $res=($stmt->execute())?$stmt->fetchAll(PDO::FETCH_OBJ): null;
+        $dbh = null;
+        return $res;
+      } catch (PDOException $e) {
+          print "Erreur !: " . $e->getMessage() . "<br/>";
+          die();
+      }
+    }
+    public function delete($id){
+      $sql='UPDATE employee AS E SET E.CurrentFlag=0 WHERE E.EmployeeID=:id';
+      try {
+        $dbh = new PDO('mysql:host=localhost;dbname=adw;charset=utf8', 'root', '');
+        $stmt=$dbh->prepare($sql);
+        $stmt->bindParam(":id",$id);
+        $res=($stmt->execute())?true: false;
+        $dbh = null;
+        return $res;
+      } catch (PDOException $e) {
+          print "Erreur !: " . $e->getMessage() . "<br/>";
+          die();
+      }
+    }
+}
+?>
